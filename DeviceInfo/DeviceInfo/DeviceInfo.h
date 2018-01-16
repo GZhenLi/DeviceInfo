2da1c15f33ba11759508c5ec94fa7992e9867fc6
gzhenli <gzhenlichina@163.com>
2018-01-16 17:35:28 +0800

    0.0.1

---
+++ DeviceInfo/DeviceInfo/DeviceInfo.h
@@ -0,0 +1,13 @@
+//
+//  DeviceInfo.h
+//  DeviceInfo
+//
+//  Created by 郭振礼 on 2018/1/16.
+//  Copyright © 2018年 郭振礼. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DeviceInfo : NSObject
+
+@end
